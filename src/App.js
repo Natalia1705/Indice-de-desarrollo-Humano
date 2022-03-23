0b5226c0e08551a6f648747a44f45ab6ca7a852b
Natalia <lau.nat.her.her@gmail.com>
2022-03-22 19:08:45 -0600

    Init project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title">Índice de desarrollo Humano</div>
+    </div>
+  );
+}
+
+export default App;
